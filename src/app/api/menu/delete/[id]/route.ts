222cc669bdd3857a62e2933b71465f9ccfa9f898
Yash Kotadia <yashkotadia2@gmail.com>
2024-12-28 13:11:25 +0530

    made changes to delete api param type

--- src/app/api/menu/delete/[id]/route.ts
+++ src/app/api/menu/delete/[id]/route.ts
@@ -1,16 +1,16 @@
-// app/api/menu/[id]/route.ts
 import { NextResponse } from "next/server";
 import Menu from "@/models/menu.model"; // Adjust path based on your project structure
 import dbConnect from "@/lib/dbConnect"; // Assuming you have a DB connection utility
 
+// DELETE handler for deleting a menu item
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   try {
     await dbConnect(); // Connect to the database
 
-    const { id } = params; // Extract the menu id from the route
+    const { id } = context.params; // Extract the menu id from the route parameters
 
     // Find and delete the menu item by id
     const deletedMenu = await Menu.findByIdAndDelete(id);
