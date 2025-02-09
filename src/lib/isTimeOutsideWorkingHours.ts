c0eb9d6fc946517322640d5546bf28c62a65ae24
Yash Kotadia <yashkotadia2@gmail.com>
2025-02-09 11:12:36 +0530

    modified DayjsTimeZoneGuess log

--- src/lib/isTimeOutsideWorkingHours.ts
+++ src/lib/isTimeOutsideWorkingHours.ts
@@ -8,8 +8,6 @@ dayjs.extend(customParseFormat);
 dayjs.extend(timezone);
 
 export const isTimeOutsideWorkingHours = (time: string): boolean => {
-  console.log("DayjsTimeZoneGuess", dayjs.tz.guess());
-
   // Get start and end time from environment variables, default to fallback if not set
   const startTime = process.env.START_TIME || "09:00"; // Default start time: 9:00 AM
   const endTime = process.env.END_TIME || "18:00"; // Default end time: 6:00 PM
@@ -17,6 +15,14 @@ export const isTimeOutsideWorkingHours = (time: string): boolean => {
   // Convert the times to dayjs objects for comparison
   const workingStartTime = dayjs(startTime, "HH:mm");
   const workingEndTime = dayjs(endTime, "HH:mm");
+
+  console.log(
+    "DayjsTimeZoneGuess",
+    dayjs.tz.guess(),
+    workingStartTime,
+    workingEndTime,
+    dayjs
+  );
 
   // Check if the time is outside the working hours (inclusive)
   return !dayjs(time).isBetween(
