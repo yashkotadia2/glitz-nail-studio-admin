342e49aa7aec703eb356455131f68c633c240e1f
Yash Kotadia <yashkotadia2@gmail.com>
2025-02-22 23:11:50 +0530

    fixed bug in isTimeOutsideWorkingHours where it used to consider prev date before T

--- src/lib/isTimeOutsideWorkingHours.ts
+++ src/lib/isTimeOutsideWorkingHours.ts
@@ -1,9 +1,11 @@
 import dayjs from "dayjs";
 import isBetween from "dayjs/plugin/isBetween";
 import customParseFormat from "dayjs/plugin/customParseFormat";
+import timezone from "dayjs/plugin/timezone";
 
 dayjs.extend(isBetween);
 dayjs.extend(customParseFormat);
+dayjs.extend(timezone);
 
 export const isTimeOutsideWorkingHours = (time: string): boolean => {
   // Get start and end time from environment variables, default to fallback if not set
@@ -14,8 +16,13 @@ export const isTimeOutsideWorkingHours = (time: string): boolean => {
   const workingStartTime = dayjs(startTime, "HH:mm");
   const workingEndTime = dayjs(endTime, "HH:mm");
 
+  // Extract time part from the input and append today's date
+  const inputTime = time.split("T")[1];
+  const today = dayjs().format("YYYY-MM-DD"); // Get today's date in YYYY-MM-DD format
+  const todayWithTime = dayjs(`${today}T${inputTime}`); // Combine today's date with extracted time
+
   // Check if the time is outside the working hours (inclusive)
-  return !dayjs(time).isBetween(
+  return !todayWithTime.isBetween(
     workingStartTime,
     workingEndTime,
     "minute",
